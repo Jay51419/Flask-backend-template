1835084e5b01f8dc440826dda7bd260a8ffda5a0
Jay51419 <jaygandhi51419@gmail.com>
2021-04-10 04:07:55 +0530

    Added login

--- app/auth/login.py
+++ app/auth/login.py
@@ -5,5 +5,14 @@ from flask import request,jsonify
 @app.route("/login",methods=['POST'])
 def login():
 	data = request.get_json()
-
-	return "Welcome to login page"
+	print(data)
+	try:
+		user = db["users"].find_one({"email":data["email"]})
+		print(user)
+		if user:
+			return jsonify({"message":"Login successful"}) if data["password"] == user["password"] else jsonify({"message":"Incorrect password"})
+		else:
+			return jsonify({"message":"User not found with this email."}) 
+	except Exception as e:
+		print(e)
+		return jsonify({"message":"Something went wrong while logging in"})

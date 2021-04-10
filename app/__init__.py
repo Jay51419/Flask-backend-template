3b531529030c54f00e62382cfda0f9b8b179ceab
Jay51419 <jaygandhi51419@gmail.com>
2021-04-10 12:42:30 +0530

    Added cors

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,10 @@ import pymongo
 import bcrypt
 import json
 from bson import ObjectId
-app = Flask(__name__)
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 mongo_uri= "mongodb://new-user:Football123@cluster0-shard-00-00.aydzz.mongodb.net:27017,cluster0-shard-00-01.aydzz.mongodb.net:27017,cluster0-shard-00-02.aydzz.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-43ghe5-shard-0&authSource=admin&retryWrites=true&w=majority"
 mongodb_client = PyMongo(app,mongo_uri)
 db = mongodb_client.db
